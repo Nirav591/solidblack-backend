05d84ed15543545ac47663ecb87639e5c16a6334
Nirav591 <nirav.v.lathiya@gmail.com>
2025-01-03 16:43:54 +0000

    add

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',        // Default MySQL username in XAMPP
-    password: '',        // Default MySQL password is empty in XAMPP
+    host: 'ls-e2029d66e023eb4cf5366724fe912a2e37ded960.czmq42a8qw3q.eu-west-2.rds.amazonaws.com',
+    user: 'dbmasteruser',        // Default MySQL username in XAMPP
+    password: 'London1995',        // Default MySQL password is empty in XAMPP
     database: 'solidblack',  // Your database name
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
